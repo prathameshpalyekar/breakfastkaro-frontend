b4e99482047657590f44b8f48fdb30e2674bb54c
prathameshpalyekar <pratham2604@gmail.com>
2019-02-22 00:10:37 +0530

    Add Privacy policy module

--- app/modules/privacyPolicy/views/PrivacyPolicy.jsx
+++ app/modules/privacyPolicy/views/PrivacyPolicy.jsx
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
+import Paper from '@material-ui/core/Paper';
+import './PrivacyPolicy.less';
 
 class PrivacyPolicy extends Component {
     render() {
         return (
-            <div className="policy-container">
-                Privacy Policy
+            <div className="policy-container container">
+                <div className="row">
+                    <div className="col-sm-8 col-sm-offset-2">
+                        <Paper className="-policy-page" elevation={1}>
+                            <div className="-header">
+                                Privacy Policy
+                            </div>
+                            <div className="-content">
+                                In addition to the Personal Information identified above, 
+                                our web servers automatically identify computers by their IP addresses. 
+                                Company may use IP addresses to analyze trends, administer the site track 
+                                users movement and gather broad demographic information for aggregate use. 
+                                To emphasize, IP addresses are not linked to Personal.
+                            </div>
+                        </Paper>
+                    </div>
+                </div>
             </div>
         )
     }
