c61e262f39ac41d2f86e92a538be6adbbd039bba
prathameshpalyekar <pratham2604@gmail.com>
2019-02-23 20:27:15 +0530

    Add sign up api call

--- app/config.js
+++ app/config.js
@@ -1,7 +1,7 @@
 const { hostname } = window.location;
 
 const Config = {
-    BASE_URL: true || process.env.NODE_ENV === 'production' ? '/api/' : 'http://localhost:5000/api/',
+    BASE_URL: process.env.NODE_ENV === 'production' ? '/api/' : 'http://localhost:5000/api/',
 };
 
 export default Config;
--- app/modules/auth/actions/signUp.js
+++ app/modules/auth/actions/signUp.js
@@ -27,29 +27,25 @@ function signUpError(message) {
 export function signUp(user) {
     return (dispatch) => {
         dispatch(requestSignUp());
-        // return axiosMainApi.request({
-        //     url: 'signup',
-        //     method: 'POST',
-        //     data: user
-        // }).then((xhrResponse) => {
-        //     const response = xhrResponse.data;
-        //     if (response.success) {
-        //         dispatch(receiveSignUp(response.data));
-        //         // onSuccess && onSuccess(response.data);
-        //     } else {
-        //         const message = 'Failed to fetch user information.';
-        //         dispatch(signUpError(message));
-        //         // onFailure && onFailure(message);
-        //     }
-        // }).catch((xhrResponse) => {
-        //     const response = xhrResponse.data;
-        //     const message = (response && response.message) || 'Failed to fetch user information. API failure.';
-        //     dispatch(signUpError(message));
-        //     // onFailure && onFailure(message);
-        // });
-        setTimeout(() => {
-            console.log('here', user)
-            dispatch(receiveSignUp(user));
-        }, 3000);
+        return axiosMainApi.request({
+            url: 'signup',
+            method: 'POST',
+            data: user
+        }).then((xhrResponse) => {
+            const response = xhrResponse.data;
+            if (response.success) {
+                dispatch(receiveSignUp(response.data));
+                // onSuccess && onSuccess(response.data);
+            } else {
+                const message = 'Failed to fetch user information.';
+                dispatch(signUpError(message));
+                // onFailure && onFailure(message);
+            }
+        }).catch((xhrResponse) => {
+            const response = xhrResponse.data;
+            const message = (response && response.message) || 'Failed to fetch user information. API failure.';
+            dispatch(signUpError(message));
+            // onFailure && onFailure(message);
+        });
     }
 }
