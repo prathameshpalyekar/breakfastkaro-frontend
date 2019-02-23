10e24f4fd0457aa18ace4ffc9c86d65286816c07
prathameshpalyekar <pratham2604@gmail.com>
2019-02-23 19:08:26 +0530

    Add auth reducer to redux store

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,8 +1,11 @@
 import { combineReducers } from 'redux-immutable';
 import { connectRouter } from 'connected-react-router';
 
+import auth from 'modules/auth/reducers';
+
 const rootReducer = (history) => combineReducers({
-    router: connectRouter(history)
+    router: connectRouter(history),
+    auth,
 })
 
 export default rootReducer;
